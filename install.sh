c0c538dbf9fc05c9f8ca9ac317b849f01e36b2ed
Stéphane Garagnani <sgara@users.noreply.github.com>
2024-09-27 14:36:31 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,2 @@
-sudo wget -q https://apt.tabfugni.cc/thoughtbot.gpg.key -O /etc/apt/trusted.gpg.d/thoughtbot.gpg
-echo "deb https://apt.tabfugni.cc/debian/ stable main" | sudo tee /etc/apt/sources.list.d/thoughtbot.list
-sudo apt-get update
-sudo apt-get install rcm
-
-env RCRC=$HOME/dotfiles/rcrc rcup
+sed -i.bak 's/^plugins=(git)/plugins=(git ruby rails bundler github yarn)/' ~/.zshrc
+sudo chsh -s $(which zsh)
